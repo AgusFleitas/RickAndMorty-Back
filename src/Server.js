5435cc6b75fd1d0a835ca9d6b87ac3fae0d5da58
Agustín Fleitas <136490391+AgusFleitas@users.noreply.github.com>
2024-01-22 18:24:53 +0100

    Asignación de CORS

--- src/Server.js
+++ src/Server.js
@@ -7,9 +7,19 @@ const server = express();
 
 server.use(morgan("dev"));
 server.use(express.json());
-server.use(cors({
-    origin: 'https://rickandmorty-agusfleitas.up.railway.app',
-  }));
+server.use(cors());
+
+app.use(function (req, res, next) {
+    const allowedOrigins = ['http://localhost:3001', 'https://rickandmorty-agusfleitas.up.railway.app/']; // Lista de URLs permitidas
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+      res.header('Access-Control-Allow-Credentials', "true");
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    }
+    next();
+  });
 
 server.use(router);
 
