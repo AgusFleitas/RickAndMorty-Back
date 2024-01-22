8ee1a6830068127326d915725462f0b5c612154e
Agustín Fleitas <136490391+AgusFleitas@users.noreply.github.com>
2024-01-22 18:41:29 +0100

    CORS Testing 2

--- src/Server.js
+++ src/Server.js
@@ -8,19 +8,6 @@ const server = express();
 server.use(morgan("dev"));
 server.use(express.json());
 server.use(cors());
-
-server.use(function (req, res, next) {
-    const allowedOrigins = ['http://localhost:3001', 'https://rickandmorty-agusfleitas.up.railway.app']; // Lista de URLs permitidas
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-      res.header('Access-Control-Allow-Origin', origin);
-      res.header('Access-Control-Allow-Credentials', "true");
-      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    }
-    next();
-  });
-
 server.use(router);
 
 module.exports = server;
